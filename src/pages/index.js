8c8609cdbcd75a16cb61dd2c1085e6ed36137d94
さ METOUSHELA. さ <168326500+MeganAe@users.noreply.github.com>
2024-09-08 21:25:05 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -167,4 +167,4 @@ const IndexPage = () => {
 
 export default IndexPage;
 
-export const Head = () => <title>Home Page</title>;
+export const Head = () => <title>Gasby Page Blog By metoushela walker</title>;
